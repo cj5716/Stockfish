7b9a5ad32f79d31c36523b8b973302ff8ab08634
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-03-26 13:43:04 +0800

    Speedup search.cpp
    Extension of https://github.com/dsmsgms/Stockfish/commit/575352e5a85cff621285c4d276ad8a520636b489

--- src/search.cpp
+++ src/search.cpp
@@ -680,7 +680,7 @@ namespace {
             {
                 thisThread->tbHits.fetch_add(1, std::memory_order_relaxed);
 
-                int drawScore = TB::UseRule50 ? 1 : 0;
+                int drawScore = TB::UseRule50;
 
                 // use the range VALUE_MATE_IN_MAX_PLY to VALUE_TB_WIN_IN_MAX_PLY to score
                 value =  wdl < -drawScore ? VALUE_MATED_IN_MAX_PLY + ss->ply + 1
@@ -898,17 +898,13 @@ namespace {
 
     // Step 11. If the position is not in TT, decrease depth by 3.
     // Use qsearch if depth is equal or below zero (~9 Elo)
-    if (    PvNode
-        && !ttMove)
-        depth -= 3;
+
+    depth -= 3 * (PvNode && !ttMove);
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
 
-    if (    cutNode
-        &&  depth >= 7
-        && !ttMove)
-        depth -= 2;
+    depth -= 2 * (cutNode && depth >= 7 && !ttMove);
 
 moves_loop: // When in check, search starts here
 
@@ -1159,43 +1155,32 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction if position is or has been on the PV
       // and node is not likely to fail low. (~3 Elo)
-      if (   ss->ttPv
-          && !likelyFailLow)
-          r -= 2;
+      r -= 2 * (ss->ttPv && !likelyFailLow);
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
-      if ((ss-1)->moveCount > 7)
-          r--;
+      r -= ((ss-1)->moveCount > 7);
 
       // Increase reduction for cut nodes (~3 Elo)
-      if (cutNode)
-          r += 2;
+      r += 2 * cutNode;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
-      if (ttCapture)
-          r++;
+      r += ttCapture;
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
       if (PvNode)
           r -= 1 + 12 / (3 + depth);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-      if (singularQuietLMR)
-          r--;
+      r -= singularQuietLMR;
 
       // Decrease reduction if we move a threatened piece (~1 Elo)
-      if (   depth > 9
-          && (mp.threatenedPieces & from_sq(move)))
-          r--;
+      r -= (depth > 9 && (mp.threatenedPieces & from_sq(move)));
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
-      if ((ss+1)->cutoffCnt > 3)
-          r++;
+      r += ((ss+1)->cutoffCnt > 3);
 
       // Decrease reduction if move is a killer and we have a good history (~1 Elo)
-      if (move == ss->killers[0]
-          && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
-          r--;
+      r -= (move == ss->killers[0] && (*contHist[0])[movedPiece][to_sq(move)] >= 3722);
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
@@ -1239,8 +1224,7 @@ moves_loop: // When in check, search starts here
               if (newDepth > d)
                   value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
 
-              int bonus = value > alpha ?  stat_bonus(newDepth)
-                                        : -stat_bonus(newDepth);
+              int bonus = (2 * (value > alpha) - 1) * stat_bonus(newDepth);
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
@@ -1250,8 +1234,7 @@ moves_loop: // When in check, search starts here
       else if (!PvNode || moveCount > 1)
       {
           // Increase reduction for cut nodes and not ttMove (~1 Elo)
-          if (!ttMove && cutNode)
-              r += 2;
+          r += (!ttMove && cutNode)*2;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
