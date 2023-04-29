bb213aede6a3c7a005de6ea33ed2bfe62684249e
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-04-29 08:40:03 +0800

    Bench 3714847
    Tune rather speculative idea to scale from 6 to 2, 25% TP

--- src/search.cpp
+++ src/search.cpp
@@ -37,7 +37,9 @@
 #include "nnue/evaluate_nnue.h"
 
 namespace Stockfish {
-
+int v1 = 0, v2 = 96, v3 = 32;
+TUNE(SetRange(-50,50),v1);
+TUNE(v2,v3);
 namespace Search {
 
   LimitsType Limits;
@@ -1065,7 +1067,7 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - (3 + 2 * (ss->ttPv && !PvNode)) * depth / 2;
+              Value singularBeta = ttValue - ((v1 * thisThread->completedDepth + v2) / v3 + 2 * (ss->ttPv && !PvNode)) * depth / 2;
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
