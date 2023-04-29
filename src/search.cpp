f75aad0d28364d4498478d90d842eba38b6085c4
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-04-29 11:29:28 +0800

    Bench 3714847
    Tune rather speculative idea to scale from 6 to 2, 25% TP

--- src/search.cpp
+++ src/search.cpp
@@ -1067,7 +1067,7 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - ((v1 * thisThread->completedDepth + v2) / v3 + 2 * (ss->ttPv && !PvNode)) * depth / 2;
+              Value singularBeta = ttValue - (v1 * thisThread->completedDepth + v2 + v3 * (ss->ttPv && !PvNode)) * depth / 32;
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
