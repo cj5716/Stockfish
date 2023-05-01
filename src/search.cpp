4bf326100e85cec9dccf97be869256968593369d
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-02 07:29:23 +0800

    Bench 3688517
    Decrease reduction for pawn promotions

--- src/search.cpp
+++ src/search.cpp
@@ -1172,6 +1172,10 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      // Decrease reduction for pawn promotions
+      if (type_of(movedPiece) == PAWN && type_of(move) == PROMOTION)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
