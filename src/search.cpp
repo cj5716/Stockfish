9affc278e996276a16acb4a81b70c545d1573963
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-04 12:04:18 +0800

    Revert "Bench 3852072"
    This reverts commit 298bda963c1adfe36b34ce63cf84d3bcb16d8f67.

--- src/search.cpp
+++ src/search.cpp
@@ -1150,20 +1150,16 @@ moves_loop: // When in check, search starts here
       if ((ss-1)->moveCount > 7)
           r--;
 
-
+      // Increase reduction for cut nodes (~3 Elo)
+      if (cutNode)
+          r += 2;
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
-      if (PvNode)
+      else if (PvNode)
           r -= 1 + 12 / (3 + depth);
 
-      else {
-          if (more_than_one(pos.checkers()))
-              r--;
-
-          // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode)
-              r += 2;
-      }
+      else if (more_than_one(pos.checkers()))
+          r--;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
