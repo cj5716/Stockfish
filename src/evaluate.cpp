3226f18a53d59f2a271096117d80be42d3dbe8c8
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-06 21:34:50 +0800

    Bench 3200242
    30k at 30+0.3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,8 +191,8 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold1    =  Value(3622);
-  constexpr Value LazyThreshold2    =  Value(1962);
+  constexpr Value LazyThreshold1    =  Value(3691);
+  constexpr Value LazyThreshold2    =  Value(1935);
   constexpr Value SpaceThreshold    =  Value(11551);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
@@ -1063,7 +1063,7 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int nnueComplexity;
-      int scale = 967 + pos.non_pawn_material() / 64;
+      int scale = 961 + (31 * pos.non_pawn_material() + 2 * pos.count<PAWN>()) / 2048;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
