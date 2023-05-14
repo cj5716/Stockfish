87f6c644ee1d1eed41d07ef6e8f855a0a5e7c6d2
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-14 15:24:00 +0800

    Bench 4077949
    Simplify negative extensions

--- src/search.cpp
+++ src/search.cpp
@@ -1094,7 +1094,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
-                  extension = -2 - !PvNode;
+                  extension = -3;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= value)
