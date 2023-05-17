2d7f07993d300c938aa3b3627508dd1e49614c35
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-17 21:09:22 +0800

    Bench 3808503
    17k games at 30+0.3

--- src/search.cpp
+++ src/search.cpp
@@ -1572,7 +1572,7 @@ moves_loop: // When in check, search starts here
           continue;
 
       // Do not search moves with bad enough SEE values (~5 Elo)
-      if (!pos.see_ge(move, Value(-110)))
+      if (!pos.see_ge(move, Value(-(PvNode ? (1709 + 75 * depth) : (1782 + 16 * depth)) / 16)))
           continue;
     }
 
