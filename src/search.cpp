941e98796e93cd8f45295b19e3db834b26f65a29
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-18 10:38:59 +0800

    Bench 3415506
    Take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1150,7 +1150,7 @@ moves_loop: // When in check, search starts here
       // Increase reduction for cut nodes (~3 Elo)
 	  // Increase further if position is not in TT.
       if (cutNode)
-          r += 2 + !ttMove;
+          r += ttMove ? 1 : 3;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
