b5172dc9de0e185fe509138518c267e1754c853d
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-27 11:00:38 +0800

    Bench 2541211
    Super short tune

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -58,6 +58,8 @@ using namespace std;
 
 namespace Stockfish {
 
+int v1 = 397, v2 = 477;
+TUNE(v1, v2);
 namespace Eval {
 
   bool useNNUE;
@@ -1071,8 +1073,8 @@ Value Eval::evaluate(const Position& pos) {
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
       // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  397 * nnueComplexity
-                        + (477 + optimism) * abs(psq - nnue)
+      nnueComplexity = (  v1 * nnueComplexity
+                        + v2 * abs(psq - nnue)
                         ) / 1024;
 
       optimism += optimism * nnueComplexity / 256;
