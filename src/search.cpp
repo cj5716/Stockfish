a3b0cbf226b06ae0dbb8e50070c533e3cc132bb1
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-27 20:58:30 +0800

    Bench 2642062
    Take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1165,6 +1165,9 @@ moves_loop: // When in check, search starts here
 
       else if (move == ttMove)
           r--;
+      
+      if (move == ss->killers[0])
+          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
