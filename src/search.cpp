6d793169d0b464eb945e388e1a72a825396c7fcb
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-05-28 09:52:56 +0800

    Bench 2506229
    sB experiment, rebased on master

--- src/search.cpp
+++ src/search.cpp
@@ -1057,7 +1057,7 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - (99 + 65 * (ss->ttPv && !PvNode)) * depth / 64;
+              Value singularBeta = ttValue - (5 + 4 * (ss->ttPv && !PvNode)) * depth / 4;
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
