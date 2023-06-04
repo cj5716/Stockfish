5dc8f85c174f697a508289046d4e0dd07f90d907
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-04 11:41:41 +0800

    Bench 2654246
    60k 30+0.3

--- src/search.cpp
+++ src/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop: // When in check, search starts here
           r += 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
-      if (ttCapture)
+      if (ttCapture && depth < 9)
           r++;
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
