085045fad4a21462043f6452e3d9bd4dd406f313
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-09 21:09:21 +0800

    Bench 2513346
    150k at 30+0.3

--- src/search.cpp
+++ src/search.cpp
@@ -1139,7 +1139,7 @@ moves_loop: // When in check, search starts here
           r -= 1 + 12 / (3 + depth);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-      if (singularQuietLMR)
+      if (singularQuietLMR && depth > 8)
           r--;
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
