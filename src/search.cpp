6f9ce92c8ea3ddec5d0de173690972a8f57b75c7
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-09 10:13:02 +0800

    Bench 2407889
    Tuned 150k at 30+0.3

--- src/search.cpp
+++ src/search.cpp
@@ -1136,7 +1136,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
       if (PvNode)
-          r -= 1 + 12 / (3 + depth);
+          r -= 1 + (depth < 9);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
