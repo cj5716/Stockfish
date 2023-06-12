b2ed071bedb5095075d0516eb27ba23edae41188
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-12 12:02:27 +0800

    Bench 2785679
    insane improvement

--- src/search.cpp
+++ src/search.cpp
@@ -751,6 +751,7 @@ namespace {
     // margin and the improving flag are used in various pruning heuristics.
     improvement =   (ss-2)->staticEval != VALUE_NONE ? ss->staticEval - (ss-2)->staticEval
                   : (ss-4)->staticEval != VALUE_NONE ? ss->staticEval - (ss-4)->staticEval
+                  : (ss-6)->staticEval != VALUE_NONE ? ss->staticEval - (ss-6)->staticEval
                   :                                    173;
     improving = improvement > 0;
 
