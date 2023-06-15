daabda626ccc79fde457c125131d541ddd27abab
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-15 20:45:46 +0800

    Bench 2728054
    Take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1095,7 +1095,7 @@ moves_loop: // When in check, search starts here
                   return singularBeta;
               
               else if (cutNode)
-                  extension = -3;
+                  extension = -2 - (ttValue >= beta);
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
