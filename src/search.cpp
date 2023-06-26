4f55caf8493a6bb05a4e25bdc60b642f4e7adf6a
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-26 14:01:32 +0800

    Bench 2577953
    take 2

--- src/search.cpp
+++ src/search.cpp
@@ -409,7 +409,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 3;
+              delta += delta / std::clamp((rootDepth + 2) / 3, 1, 4);
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
