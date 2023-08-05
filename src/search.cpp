71a1c10403d5c9fa823e27cfc94dad2b6b11a7a5
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-05 14:11:24 +0800

    Bench 1433439
    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -754,14 +754,9 @@ namespace {
                 : true;
 
     // Step 7. Razoring (~1 Elo).
-    // If eval is really low check with qsearch if it can exceed alpha, if it can't,
-    // return a fail low.
+    // If eval is really low return a fail low.
     if (eval < alpha - 456 - 252 * depth * depth)
-    {
-        value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
-        if (value < alpha)
-            return value;
-    }
+        return alpha;
 
     // Step 8. Futility pruning: child node (~40 Elo).
     // The depth condition is important for mate finding.
