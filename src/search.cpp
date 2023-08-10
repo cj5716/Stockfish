cf07fe430ee84c1a15c29d44e8dd599f3bdd1a6e
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-10 10:56:20 +0800

    Bench 1496460
    take 5

--- src/search.cpp
+++ src/search.cpp
@@ -1095,7 +1095,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
-                  extension = -2 - !PvNode;
+                  extension = -2 - !cutNode;
 
               // If we are on a cutNode, reduce it based on depth (negative extension) (~1 Elo)
               else if (cutNode)
