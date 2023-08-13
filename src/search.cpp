0c7d4e7b34a28ffa0f6d9c02a9dd200011e2ad77
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-13 08:04:34 +0800

    Bench 1466059
    take 4

--- src/search.cpp
+++ src/search.cpp
@@ -1168,7 +1168,7 @@ moves_loop: // When in check, search starts here
           r++;
 
       else if (move == ttMove)
-          r -= 2;
+          r -= tte->depth() >= depth + 3 ? 2 : 1;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
