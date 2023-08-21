995b0dc4c77e854ec667bad02dba26d9f5276c2a
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-21 16:27:27 +0800

    Bench 1768704
    same as take 1 but double effect

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
           r++;
 
       else if (move == ttMove)
-          r--;
+          r -= likelyFailLow ? 1 : 3;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
