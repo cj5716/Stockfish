c63a6f534328af4de31cad06de71777e22e1dc36
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-09-01 13:31:38 +0800

    Final take of this idea (effect at higher depth). Bench 1426295

--- src/search.cpp
+++ src/search.cpp
@@ -834,7 +834,7 @@ namespace {
     if (    cutNode
         &&  depth >= 8
         && !ttMove)
-        depth -= 2;
+        depth -= 2 + 3 * (ss->ttHit && tte->depth() >= depth + 3);
 
     probCutBeta = beta + 168 - 61 * improving;
 
