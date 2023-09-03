032f3474ada888e6559bad22bfa6fc3dc0ff2944
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-09-03 08:41:16 +0800

    Take 2. Bench 1627767

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::clamp(336 * d - 547, 0, 1561);
+    return std::clamp(336 * d - 547, 32, 1561);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
