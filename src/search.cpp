c13831e1ff1364c39174a832ce6a233fd6d65f8c
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-09-14 15:54:03 +0800

    Take 1. Bench 1705063

--- src/search.cpp
+++ src/search.cpp
@@ -1328,7 +1328,7 @@ moves_loop: // When in check, search starts here
 
 
       // If the move is worse than some previously searched move, remember it, to update its stats later
-      if (move != bestMove)
+      if (bestMove && move != bestMove)
       {
           if (capture && captureCount < 32)
               capturesSearched[captureCount++] = move;
