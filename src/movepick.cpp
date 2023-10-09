d53262e71538f1dbd1c7de6e7dbdefaab5d0b95b
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-10-09 21:29:01 +0800

    Take 2. Bench 1296407

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -144,7 +144,7 @@ void MovePicker::score() {
           Square    to   = to_sq(m);
 
           // histories
-          m.value =  2 * (*mainHistory)[pos.side_to_move()][bool(oppThreats & to)][from_to(m)];
+          m.value =  3 * (*mainHistory)[pos.side_to_move()][bool(oppThreats & to)][from_to(m)];
           m.value += 2 * (*continuationHistory[0])[pc][to];
           m.value +=     (*continuationHistory[1])[pc][to];
           m.value +=     (*continuationHistory[3])[pc][to];
