80de92c1ef1cebdc608574a214dfa0f0b9cd08bc
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-10-12 21:09:46 +0800

    Take 1. Bench 1174751

--- src/search.cpp
+++ src/search.cpp
@@ -894,6 +894,9 @@ namespace {
 
                 if (value >= probCutBeta)
                 {
+                    // Update capture histories with current move
+                    thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] << stat_bonus(depth - 3);
+
                     // Save ProbCut data into transposition table
                     tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3, move, ss->staticEval);
                     return value;
