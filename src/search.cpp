1176d57a20574c86c4d1d606a8083b91c8daec0b
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-10-21 17:01:26 +0800

    Take 1. Bench 1199928

--- src/search.cpp
+++ src/search.cpp
@@ -916,7 +916,7 @@ moves_loop: // When in check, search starts here
         && ttValue >= probCutBeta
         && abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY
         && abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
-        return probCutBeta;
+        return ttValue - (probCutBeta - beta);
 
     const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory,
                                          (ss-3)->continuationHistory, (ss-4)->continuationHistory,
