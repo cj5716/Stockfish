43d0f6cd94055ea146b78e24e9ef36e68b1e0a47
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-10-24 08:10:35 +0800

    Take 2. Bench 1198503

--- src/search.cpp
+++ src/search.cpp
@@ -598,7 +598,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
 
     (ss + 1)->excludedMove = bestMove = MOVE_NONE;
-    (ss + 3)->killers[0] = (ss + 3)->killers[1] = MOVE_NONE;
+    (ss + 3)->killers[0] = (ss + 2)->killers[1] = MOVE_NONE;
     (ss + 2)->cutoffCnt                         = 0;
     ss->doubleExtensions                        = (ss - 1)->doubleExtensions;
     Square prevSq = is_ok((ss - 1)->currentMove) ? to_sq((ss - 1)->currentMove) : SQ_NONE;
