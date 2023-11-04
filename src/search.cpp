0aa041a5e85220a703061a024e7c6e01edc56c35
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-04 19:14:35 +0800

    Last test with this, VLTC low TP. Bench 1365515

--- src/search.cpp
+++ src/search.cpp
@@ -1038,7 +1038,7 @@ moves_loop:  // When in check, search starts here
                 && abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && (tte->bound() & BOUND_LOWER)
                 && tte->depth() >= depth - 3)
             {
-                Value singularBeta  = ttValue - (64 + 57 * (ss->ttPv && !PvNode)) * depth / 64;
+                Value singularBeta  = ttValue - (3 + 4 * (ss->ttPv && !PvNode)) * depth / 4;
                 Depth singularDepth = (depth - 1) / 2;
 
                 ss->excludedMove = move;
