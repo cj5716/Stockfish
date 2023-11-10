dbe6b39bbca711e4d90bb5b17bf7cef8d8098ca7
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-10 20:29:43 +0800

    Retry take 2 with new pawn hist patch. Bench 1398339

--- src/search.cpp
+++ src/search.cpp
@@ -746,7 +746,9 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     // Use static evaluation difference to improve quiet move ordering (~4 Elo)
     if (is_ok((ss - 1)->currentMove) && !(ss - 1)->inCheck && !priorCapture)
     {
-        int bonus = std::clamp(-18 * int((ss - 1)->staticEval + ss->staticEval), -1812, 1812);
+        int bonus = std::clamp(-51 * int((ss - 1)->staticEval + ss->staticEval)
+                                 * abs((ss - 1)->staticEval + ss->staticEval) / 151,
+                               -1812, 1812);
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
         if (type_of(pos.piece_on(prevSq)) != PAWN && type_of((ss - 1)->currentMove) != PROMOTION)
             thisThread->pawnHistory[pawn_structure(pos)][pos.piece_on(prevSq)][prevSq] << bonus / 4;
