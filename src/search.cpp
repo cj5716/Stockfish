5a8c7331bfa252a4ff1ee68b0721e3652fef44ce
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-14 18:44:26 +0800

    Take 1. Bench 1203487

--- src/search.cpp
+++ src/search.cpp
@@ -898,9 +898,9 @@ moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
     probCutBeta = beta + 416;
-    if (ss->inCheck && !PvNode && ttCapture && (tte->bound() & BOUND_LOWER)
-        && tte->depth() >= depth - 4 && ttValue >= probCutBeta
-        && abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
+    if (!PvNode && ttCapture && (tte->bound() & BOUND_LOWER) && tte->depth() >= depth - 3
+        && ttValue >= probCutBeta && abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY
+        && abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
         return probCutBeta;
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory,
