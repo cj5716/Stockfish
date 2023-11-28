f8dfb11b73a3262a1d410da1b35e52dc1aa9db0d
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-28 18:59:48 +0800

    Take 1. Bench 1474236

--- src/search.cpp
+++ src/search.cpp
@@ -1130,6 +1130,10 @@ moves_loop:  // When in check, search starts here
         if ((ss - 1)->moveCount > 7)
             r--;
 
+        // Decrease reduction if static eval is not a good prediction of node value (~10000 Elo)
+        if (ttValue != VALUE_NONE && abs(ss->staticEval - ttValue) >= 512)
+            r--;
+
         // Increase reduction for cut nodes (~3 Elo)
         if (cutNode)
             r += 2;
