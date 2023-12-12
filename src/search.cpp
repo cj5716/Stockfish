4dbf8a344c482cfc62e6fe3dd920a01fb426d6e4
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-12-12 10:09:52 +0800

    Take 4. Bench 1364345

--- src/search.cpp
+++ src/search.cpp
@@ -420,7 +420,7 @@ void Thread::search() {
                 }
                 else if (bestValue >= beta)
                 {
-                    if (bestValue >= beta + 2 * delta)
+                    if (bestValue >= beta + delta)
                         alpha = (alpha + beta) / 2;
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
