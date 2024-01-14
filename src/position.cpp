a8398095e0485edfe7cf70ffae45485d9db3f269
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-01-14 12:20:41 +0800

    Insufficient material detection. Bench 1390844

--- src/position.cpp
+++ src/position.cpp
@@ -1126,9 +1126,13 @@ bool Position::see_ge(Move m, int threshold) const {
     return bool(res);
 }
 
-// Tests whether the position is drawn by 50-move rule
-// or by repetition. It does not detect stalemates.
+// Tests whether the position is drawn by 50-move rule, repetition
+// or insufficient material. It does not detect stalemates.
 bool Position::is_draw(int ply) const {
+
+    // Insufficient material
+    if (count<ALL_PIECES>() <= 3 && !pieces(PAWN, ROOK, QUEEN))
+        return true;
 
     if (st->rule50 > 99 && (!checkers() || MoveList<LEGAL>(*this).size()))
         return true;
