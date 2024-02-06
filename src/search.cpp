8d3206e36aa0826634c4da37a05f91f411261ee0
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-02-06 16:43:46 +0800

    Take 11. Bench 1591434

--- src/search.cpp
+++ src/search.cpp
@@ -341,7 +341,7 @@ void Search::Worker::iterative_deepening() {
                 // re-search, otherwise exit the loop.
                 if (bestValue <= alpha)
                 {
-                    beta  = (alpha + beta) / 2;
+                    beta  = (bestValue * 4 + alpha * 2 + beta * 3) / 9;
                     alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                     failedHighCnt = 0;
