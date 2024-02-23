dd3edc225d933511d326aa5dee2a1b1d679eaa14
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-02-23 13:24:12 +0800

    Do not allow this at root. Bench 1638591

--- src/search.cpp
+++ src/search.cpp
@@ -1271,7 +1271,7 @@ moves_loop:  // When in check, search starts here
                 else
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
-                    if (depth > 2 && depth < 13 && beta < 13652 && value > -12761)
+                    if (!rootNode && depth > 2 && depth < 13 && beta < 13652 && value > -12761)
                         depth -= 2;
 
                     assert(depth > 0);
