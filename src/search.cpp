69bd8a65bedc80bd140f7f576e5c23ba21c5b5e1
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-02-26 21:36:38 +0800

    Bench 1314227.

--- src/search.cpp
+++ src/search.cpp
@@ -872,7 +872,7 @@ Value Search::Worker::search(
                               move, unadjustedStaticEval, tt.generation());
                     return std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY
                             && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
-                           ? (value + 3 * beta) / 4
+                           ? (3 * value + beta) / 4
                            : value;
                 }
             }
