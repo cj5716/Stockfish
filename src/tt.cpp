ee1c3fdac2ce660e8400c05c3b2e13cd4d5c92eb
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-02-28 18:18:57 +0800

    Nice one noticed by gabe. Bench 1612030

--- src/tt.cpp
+++ src/tt.cpp
@@ -34,10 +34,6 @@ namespace Stockfish {
 void TTEntry::save(
   Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev, uint8_t generation8) {
 
-    // Preserve any existing move for the same position
-    if (m || uint16_t(k) != key16)
-        move16 = m;
-
     // Overwrite less valuable entries (cheapest checks first)
     if (b == BOUND_EXACT || uint16_t(k) != key16 || d - DEPTH_OFFSET + 2 * pv > depth8 - 4)
     {
@@ -49,6 +45,10 @@ void TTEntry::save(
         genBound8 = uint8_t(generation8 | uint8_t(pv) << 2 | b);
         value16   = int16_t(v);
         eval16    = int16_t(ev);
+
+        // Preserve any existing move for the same position if no best move is found in this search
+        if (m || uint16_t(k) != key16)
+            move16 = m;
     }
 }
 
