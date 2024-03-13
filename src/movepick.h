7b41a851c6bfbba60067644f1c43e73c1075cb18
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-03-13 14:27:54 +0800

    Take 3. Bench 1678353

--- src/movepick.h
+++ src/movepick.h
@@ -33,8 +33,8 @@
 
 namespace Stockfish {
 
-constexpr int PAWN_HISTORY_SIZE              = 512;   // has to be a power of 2
-constexpr int CORRECTION_HISTORY_SIZE        = 8192;  // has to be a power of 2
+constexpr int PAWN_HISTORY_SIZE              = 512;    // has to be a power of 2
+constexpr int CORRECTION_HISTORY_SIZE        = 16384;  // has to be a power of 2
 constexpr int CORRECTION_HISTORY_LIMIT       = 1024;
 constexpr int CORRECTION_HISTORY_NUM_BUCKETS = 4;
 
