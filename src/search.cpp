3eaa39ab784e1dd766a947076f0befc3b04a350a
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-04-14 10:13:45 +0800

    Take 1 25 TP. Bench 1608064

--- src/search.cpp
+++ src/search.cpp
@@ -1033,7 +1033,7 @@ moves_loop:  // When in check, search starts here
                 && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && (tte->bound() & BOUND_LOWER)
                 && tte->depth() >= depth - 3)
             {
-                Value singularBeta  = ttValue - (64 + 59 * (ss->ttPv && !PvNode)) * depth / 64;
+                Value singularBeta  = ttValue - (3 + 3 * (ss->ttPv && !PvNode)) * depth / 4;
                 Depth singularDepth = newDepth / 2;
 
                 ss->excludedMove = move;
