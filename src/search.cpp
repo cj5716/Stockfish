5160adb6013a25ea28444d42e7403a2d58ce85d0
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-04-26 16:33:03 +0800

    Take 1. Bench 1631354

--- src/search.cpp
+++ src/search.cpp
@@ -1045,6 +1045,9 @@ moves_loop:  // When in check, search starts here
                   search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
                 ss->excludedMove = Move::none();
 
+                if (ttValue <= value && !ttCapture)
+                    update_quiet_stats(pos, ss, *this, ttMove, -stat_malus(singularDepth));
+
                 if (value < singularBeta)
                 {
                     extension = 1;
