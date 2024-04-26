40f1e5e99918511d9fb3ff8140e09975392be10a
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-04-26 16:34:38 +0800

    Take 2. Bench 1413143

--- src/search.cpp
+++ src/search.cpp
@@ -1045,9 +1045,6 @@ moves_loop:  // When in check, search starts here
                   search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
                 ss->excludedMove = Move::none();
 
-                if (ttValue <= value && !ttCapture)
-                    update_quiet_stats(pos, ss, *this, ttMove, -stat_malus(singularDepth));
-
                 if (value < singularBeta)
                 {
                     extension = 1;
@@ -1069,7 +1066,12 @@ moves_loop:  // When in check, search starts here
                 // we assume this expected cut-node is not singular (multiple moves fail high),
                 // and we can prune the whole subtree by returning a softbound.
                 else if (singularBeta >= beta)
+                {
+                    if (ttValue <= value && !ttCapture)
+                        update_quiet_stats(pos, ss, *this, ttMove, -stat_malus(singularDepth));
+
                     return singularBeta;
+                }
 
                 // Negative extensions
                 // If other moves failed high over (ttValue - margin) without the ttMove on a reduced search,
