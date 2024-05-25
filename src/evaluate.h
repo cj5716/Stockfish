ef4850255e357bb5ae95b615655ff1fda1a54a1d
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-05-25 13:27:10 +0800

    up if x > 255, down if x < 32. Bench 2261395

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,8 +33,8 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-c721dfca8cd3.nnue"
-#define EvalFileDefaultNameSmall "nn-baff1ede1f90.nnue"
+#define EvalFileDefaultNameBig "nn-681a3fed22c8.nnue"
+#define EvalFileDefaultNameSmall "nn-1d0d066b8b42.nnue"
 
 namespace NNUE {
 struct Networks;
