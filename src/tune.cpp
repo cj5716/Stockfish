34cc3a591aedf5d949c244015b0d7c2425ccc73f
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-06-07 22:09:11 +0800

    Take 1. Bench 1165901

--- src/tune.cpp
+++ src/tune.cpp
@@ -59,8 +59,7 @@ void make_option(OptionsMap* options, const string& n, int v, const SetRange& r)
 
     // Print formatted parameters, ready to be copy-pasted in Fishtest
     std::cout << n << "," << v << "," << r(v).first << "," << r(v).second << ","
-              << (r(v).second - r(v).first) / 20.0 << ","
-              << "0.0020" << std::endl;
+              << (r(v).second - r(v).first) / 20.0 << "," << "0.0020" << std::endl;
 }
 }
 
