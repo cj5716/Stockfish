afe41d05f2930bca3240ee4fa29e89b307ca2bc4
cj5716 <125858804+cj5716@users.noreply.github.com>
2025-10-29 10:19:41 +0800

    Bench 2366172

--- src/nnue/features/full_threats.cpp
+++ src/nnue/features/full_threats.cpp
@@ -81,8 +81,8 @@ IndexType FullThreats::make_index(Piece attkr, Square from, Square to, Piece att
 
     // Some threats imply the existence of the corresponding ones in the opposite
     // direction. We filter them here to ensure only one such threat is active.
-    if ((map[enemy][type_of(attkr) - 1][type_of(attkd) - 1] < 0)
-        || (type_of(attkr) == type_of(attkd) && (enemy || type_of(attkr) != PAWN) && from < to))
+    if ((map2[enemy][type_of(attkr) - 1][type_of(attkd) - 1] == -1)
+        || (map2[enemy][type_of(attkr) - 1][type_of(attkd) - 1] == -2 && from < to))
     {
         return Dimensions;
     }
@@ -91,7 +91,7 @@ IndexType FullThreats::make_index(Piece attkr, Square from, Square to, Piece att
 
     return IndexType(
       offsets[attkr][65]
-      + (color_of(attkd) * (numValidTargets[attkr] / 2) + map[enemy][type_of(attkr) - 1][type_of(attkd) - 1])
+      + (color_of(attkd) * (numValidTargets[attkr] / 2) + map[type_of(attkr) - 1][type_of(attkd) - 1])
           * offsets[attkr][64]
       + offsets[attkr][from] + popcount((square_bb(to) - 1) & attacks));
 }
